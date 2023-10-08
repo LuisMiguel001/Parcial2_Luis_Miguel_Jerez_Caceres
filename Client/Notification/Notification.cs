20f50c3b2d575210f4b7d03fd3e805c8c785ce51
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-10-08 01:39:00 -0400

    Agregando el regitro, consulta y la notificación.

---
+++ Client/Notification/Notification.cs
@@ -0,0 +1,22 @@
+﻿using Radzen;
+
+namespace Parcial2_Luis_Miguel_Jerez_Caceres.Client.Notification;
+
+public static class Notification
+{
+	public static void ShowNotification(this NotificationService notifier,
+		string titulo,
+		string mensaje,
+		NotificationSeverity severity)
+	{
+		var message = new NotificationMessage
+		{
+			Severity = severity,
+			Summary = titulo,
+			Detail = mensaje,
+			Duration = 3000
+		};
+
+		notifier.Notify(message);
+	}
+}
